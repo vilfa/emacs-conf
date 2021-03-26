035d5fc03d58627e355e5a146b82ff4d43e8296f
Luka Vilfan <luka.vilfan@protonmail.com>
2021-03-26 17:37:12 +0100

    install scripts and directory structure

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ -f $HOME/.emacs ]
+then
+	echo ".emacs already exists. replace (y/n)?"
+	read replace
+	if [ "$replace" == "y" ] || [ "$replace" == "Y" ] || [ "$replace" == "yes" ] 
+	then
+		rm -f $HOME/.emacs
+		cp .emacs $HOME
+		echo "replaced existing .emacs ..."
+	else
+		echo "skipped existing .emacs ..."
+	fi
+else
+	echo "no existing .emacs. create (y/n)?"
+	read create
+	if [ "$create" == "y" ] || [ "$create" == "Y" ] || [ "$create" == "yes" ]
+	then
+		cp .emacs $HOME
+		echo "created .emacs ..."
+	else
+		echo "skipped ..."
+	fi
+fi
+
+if [ -d $HOME/.emacs.d ]
+then
+	echo ".emacs.d dir already exists. replace (y/n)?"
+	read replace
+	if [ "$replace" == "y" ] || [ "$replace" == "Y" ] || [ "$replace" == "yes" ] 
+	then
+		rm -rf $HOME/.emacs.d
+		mkdir -p $HOME/.emacs.d/themes && cp -r .emacs.d $HOME/.emacs.d  
+		echo "replaced existing ~/.emacs.d ..."
+	else
+		echo "skipped existing ~/.emacs.d ..."
+	fi
+else
+	echo "no existing .emacs.d dir. create (y/n)?"
+	read create
+	if [ "$create" == "y" ] || [ "$create" == "Y" ] || [ "$create" == "yes" ]
+	then
+		mkdir -p $HOME/.emacs.d/themes && cp -r .emacs.d $HOME/.emacs.d
+		echo "created .emacs.d dir ..."
+	else
+		echo "skipped ..."
+	fi
+fi
---
+++ install_for_root.sh
@@ -0,0 +1,54 @@
+if [ ! "$(whoami)" = "root" ]
+then
+    echo "this script requires superuser privileges."
+    exit -1 
+fi
+
+if [ -f /root/.emacs ]
+then    
+    echo "root user .emacs already exists. replace (y/n)?"
+    read replace
+    if [ "$replace" == "y" ] || [ "$replace" == "Y" ] || [ "$replace" == "yes" ] 
+    then
+	rm -f /root/.emacs
+	cp .emacs /root
+	echo "replaced existing root user .emacs ..."
+    else
+	echo "skipped existing root user .emacs ..."
+    fi
+else
+    echo "no existing root user .emacs. create (y/n)?"
+    read create
+    if [ "$create" == "y" ] || [ "$create" == "Y" ] || [ "$create" == "yes" ]
+    then
+	cp .emacs /root
+	echo "created root user .emacs ..."
+    else
+	echo "skipped ..."
+    fi
+fi
+
+if [ -d /root/.emacs.d ]
+then
+    echo "root user .emacs.d dir already exists. replace (y/n)?"
+    read replace
+    if [ "$replace" == "y" ] || [ "$replace" == "Y" ] || [ "$replace" == "yes" ] 
+    then
+	rm -rf /root/.
+	mkdir -p /root/.emacs.d/themes && cp -r .emacs.d/* /root/.emacs.d
+	echo "replaced existing root user ~/.emacs.d ..."
+    else
+	echo "skipped existing root user ~/.emacs.d ..."
+    fi
+else
+    echo "no existing root user .emacs.d dir. create (y/n)?"
+    read create
+    if [ "$create" == "y" ] || [ "$create" == "Y" ] || [ "$create" == "yes" ]
+    then
+	mkdir -p /root/.emacs.d/themes && cp -r .emacs.d/* /root/.emacs.d
+	echo "created root user .emacs.d dir ..."
+    else
+	echo "skipped ..."
+    fi
+fi
+
